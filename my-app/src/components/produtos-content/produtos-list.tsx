84d32991b236b0ead43c6978a8ad0f7d3d4d5e32
Mari Spinola <mapincosf@gmail.com>
2024-03-30 22:39:01 -0300

    feat: product page data list weith JSON

---
+++ my-app/src/components/produtos-content/produtos-list.tsx
@@ -0,0 +1,40 @@
+'use client'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+const ProtudosLista = () => {
+   const [data, setData] = useState([]);
+
+   useEffect(() => {
+      const fetchData = async () => {
+         const res = await fetch('/data.json')
+         const jsonData = await res.json()
+         setData(jsonData)
+         }
+   
+         fetchData()
+      }, [])
+   
+      interface Product {
+         name: string;
+         src: string;
+      }
+
+      return (
+         <section>
+            {data.map((item: Product, index: number) => (
+               <div key={index}>
+                  <h2>{item.name}</h2>
+                  <div>
+                     <Image src={item.src} alt={item.name} 
+                     priority
+                     width={100}
+                     height={100}
+                     />
+                  </div>
+               </div>
+            ))}
+         </section>
+      )
+   };
+   
+export default ProtudosLista;
