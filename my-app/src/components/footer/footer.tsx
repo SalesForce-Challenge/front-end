42e6542d4918bc071cb1f63e2a30b77f14e8f04c
Mari Spinola <mapincosf@gmail.com>
2024-03-30 21:17:09 -0300

    fix: footer content style

--- my-app/src/components/footer/footer.tsx
+++ my-app/src/components/footer/footer.tsx
@@ -1,22 +1,21 @@
 import Image from 'next/image'
 import logo from '../../assets/icon/logo_desk.svg'
-
+import classes from './footer.module.css'
 
 const Footer = () => {
 
    return (
-      <footer>
-         <div>
+      <footer className={classes.footerWrapper}> 
+         <div className={classes.logoWrapper}>
             <Image src={logo} alt="Sales Force" />
             <p>Entre em contato 0800 891 1887</p>
          </div>
 
-         <div>
-            <div>
-               <div>
+         <div className={classes.footerLinksWrapper}>
+               <div className={classes.collumWrapper}>
                   <h4>Conheça a Salesforce</h4>
-                  <ul>
-                     <li><a href="#">O que é CRM?</a></li>
+                  <ul className={classes.collumItemWrapper}>
+                     <li ><a href="#">O que é CRM?</a></li>
                      <li><a href="#">O que é a Salesforce?</a></li>
                      <li><a href="#">Explorar todos os produtos</a></li>
                      <li><a href="#">Edições e Preços</a></li>
@@ -27,18 +26,16 @@ const Footer = () => {
                   </ul>
                </div>
 
-               <div>
-
+               <div className={classes.collumWrapper}>
                   <h4>Sobre a Salesforce</h4>
-                  <ul>
-                     <li><a href="#">Nossa História</a></li>
+                  <ul className={classes.collumItemWrapper}>
+                     <li ><a href="#">Nossa História</a></li>
                      <li><a href="#">Blog</a></li>
                      <li><a href="#">Carreira</a></li>
                      <li><a href="#">Salesforce.org</a></li>
                      <li><a href="#">Dê nos o seu feedback</a></li>
                   </ul>
                </div>
-            </div>
          </div>
       </footer>
    );
