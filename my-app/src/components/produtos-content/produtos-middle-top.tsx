e07bbdcb87abde2a39165d357413d5cb1961bb09
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2024-04-01 15:41:20 -0300

    fix: middle product style

--- my-app/src/components/produtos-content/produtos-middle-top.tsx
+++ my-app/src/components/produtos-content/produtos-middle-top.tsx
@@ -1,9 +1,16 @@
+import classes from './produtos-middle-top.module.css';
+
+
 const ProdutosMiddleTop = () => {
     return (
-        <section>
-            <h2>A inovação por trás do Customer 360</h2>
-            <p>Dados, Inteligência Artificial e CRM</p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/sCI-BZVmYoU?si=4LIbheD3Gqv472Mg" 
+        <section className={classes.sectionWrapper}>
+
+            <div className={classes.sectionWrapperTitle}>
+                <h2>A inovação por trás do Customer 360</h2>
+                <p>Dados, Inteligência Artificial e CRM</p>
+            </div>
+            
+            <iframe width="800" height="450" src="https://www.youtube.com/embed/sCI-BZVmYoU?si=4LIbheD3Gqv472Mg" 
                 title="YouTube video player" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                 referrerPolicy="strict-origin-when-cross-origin" 
