47e953cccc7cf0514ded062446c02dce82db1942
Mari Spinola <mapincosf@gmail.com>
2024-05-20 19:18:47 -0300

    fix style

--- my-app/src/app/cadastrar/page.tsx
+++ my-app/src/app/cadastrar/page.tsx
@@ -1,14 +1,17 @@
 import FormCadastro from '@/components/form-cadastro/form-cadastro'
 import MainHeader from '@/components/main-header/main-header'
 import React from 'react'
+import style from './page.module.css'
 
 const Cadastrar: React.FC = () => {
    return (
       <>
          <MainHeader />
-         <main>
-            <FormCadastro />
-         </main>
+         <div className={ style.mainBody }>
+            <main className={ style.mainWrapper }>
+               <FormCadastro />
+            </main>
+         </div>
       </>
    )
 }
--- my-app/src/components/form-cadastro/form-cadastro.tsx
+++ my-app/src/components/form-cadastro/form-cadastro.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
+import style from './from-cadastro.module.css'
 
 const FormCadastro: React.FC = () => {
    const [nome, setNome] = useState('')
@@ -13,6 +14,7 @@ const FormCadastro: React.FC = () => {
    const [response, setResponse] = useState('')
    const [senha, setSenha] = useState('')
    const [paises, setPaises] = useState([])
+
    useEffect(() => {
       const fetchData = async () => {
          try {
@@ -74,45 +76,44 @@ const FormCadastro: React.FC = () => {
    }
 
    return (
-      <section>
+      <section className={ style.cadastroSectionWrapper }>
          <h2>Cadastre-se! <span> Teste gratuito por 30 dias</span></h2>
-         <form onSubmit={ handleSubmit }>
-            <div>
-               <div>
+         <form onSubmit={ handleSubmit } className={ style.formWrapper }>
+            <div className={ style.formWrapperItemRow }>
+               <div className={ style.formWrapperItem }>
                   <label htmlFor="primeiroNome">Primeiro nome:</label>
                   <input type="text" id="primeiroNome" placeholder="Digite o seu nome" value={ nome } onChange={ (event) => setNome(event.target.value) } />
                </div>
 
-               <div>
+               <div className={ style.formWrapperItem }>
                   <label htmlFor="sobrenome">Sobrenome:</label>
                   <input type="text" id="sobrenome" placeholder="Digite o seu sobrenome" value={ sobrenome } onChange={ (event) => setSobrenome(event.target.value) } />
                </div>
             </div>
 
-            <div>
-
-               <div>
+            <div className={ style.formWrapperItemRow }>
+               <div className={ style.formWrapperItem }>
                   <label htmlFor="email">E-mail:</label>
                   <input type="email" id="email" placeholder="Digite o seu e-mail" value={ email } onChange={ (event) => setEmail(event.target.value) } />
                </div>
-               <div>
+               <div className={ style.formWrapperItem }>
                   <label htmlFor="senha">Senha:</label>
                   <input type="password" id="senha" placeholder="Digite a sua senha" value={ senha } onChange={ (event) => setSenha(event.target.value) } />
                </div>
             </div>
-            <div>
-               <div>
+            <div className={ style.formWrapperItemRow }>
+               <div className={ style.formWrapperItem }>
                   <label htmlFor="cargo">Cargo:</label>
                   <input type="text" id="cargo" placeholder="Digite o seu cargo" value={ cargo } onChange={ (event) => setCargo(event.target.value) } />
                </div>
 
-               <div>
+               <div className={ style.formWrapperItem }>
                   <label htmlFor="empresa">Empresa:</label>
                   <input type="text" id="empresa" placeholder="Digite o nome da empresa" value={ empresa } onChange={ (event) => setEmpresa(event.target.value) } />
                </div>
             </div>
 
-            <div>
+            <div className={ style.formWrapperItemColumn }>
                <label htmlFor="funcionarios">Media de Funcionários:</label>
                <select id="funcionarios" value={ funcionarios } onChange={ (event) => setFuncionarios(event.target.value) }>
                   <option value="option1">1 a 50 funcionários</option>
@@ -123,7 +124,7 @@ const FormCadastro: React.FC = () => {
                </select>
             </div>
 
-            <div>
+            <div className={ style.formWrapperItemColumn }>
                <label htmlFor="pais">País</label>
                <select id="pais" value={ pais } onChange={ (event) => setPais(event.target.value) }>
                   { paises.map((pais: any) => (
@@ -139,7 +140,7 @@ const FormCadastro: React.FC = () => {
             </label>
 
 
-            <div>
+            <div className={ style.buttonSubmitWrapper }>
                <button type="submit">Criar conta</button>
             </div>
          </form>
