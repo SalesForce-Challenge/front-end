026f33fd2100879dccc58fe7e2d6d10f46c42eb7
Mari Spinola <mapincosf@gmail.com>
2024-05-19 22:26:06 -0300

    fix: footer + aria labels

--- my-app/src/components/footer/footer.tsx
+++ my-app/src/components/footer/footer.tsx
@@ -5,40 +5,40 @@ import classes from './footer.module.css'
 const Footer = () => {
 
    return (
-      <footer className={classes.footerWrapper}> 
-         <div className={classes.logoWrapper}>
-            <Image src={logo} alt="Sales Force" />
+      <footer className={ classes.footerWrapper } aria-labelledby="footer-heading">
+         <div className={ classes.logoWrapper }>
+            <Image src={ logo } alt="Salesforce logo" />
             <p>Entre em contato 0800 891 1887</p>
          </div>
 
-         <div className={classes.footerLinksWrapper}>
-               <div className={classes.collumWrapper}>
-                  <h4>Conheça a Salesforce</h4>
-                  <ul className={classes.collumItemWrapper}>
-                     <li ><a href="#">O que é CRM?</a></li>
-                     <li><a href="#">O que é a Salesforce?</a></li>
-                     <li><a href="#">Explorar todos os produtos</a></li>
-                     <li><a href="#">Edições e Preços</a></li>
-                     <li><a href="#">História de Clientes</a></li>
-                     <li><a href="#">Investidores</a></li>
-                     <li><a href="#">Política de Privacidade</a></li>
-                     <li><a href="#">Segurança</a></li>
-                  </ul>
-               </div>
+         <div className={ classes.footerLinksWrapper }>
+            <div className={ classes.columnWrapper }>
+               <h4 id="footer-heading">Conheça a Salesforce</h4>
+               <ul className={ classes.columnItemWrapper }>
+                  <li><a href="#">O que é CRM?</a></li>
+                  <li><a href="#">O que é a Salesforce?</a></li>
+                  <li><a href="#">Explorar todos os produtos</a></li>
+                  <li><a href="#">Edições e Preços</a></li>
+                  <li><a href="#">História de Clientes</a></li>
+                  <li><a href="#">Investidores</a></li>
+                  <li><a href="#">Política de Privacidade</a></li>
+                  <li><a href="#">Segurança</a></li>
+               </ul>
+            </div>
 
-               <div className={classes.collumWrapper}>
-                  <h4>Sobre a Salesforce</h4>
-                  <ul className={classes.collumItemWrapper}>
-                     <li ><a href="#">Nossa História</a></li>
-                     <li><a href="#">Blog</a></li>
-                     <li><a href="#">Carreira</a></li>
-                     <li><a href="#">Salesforce.org</a></li>
-                     <li><a href="#">Dê nos o seu feedback</a></li>
-                  </ul>
-               </div>
-         </div>
+            <div className={ classes.columnWrapper }>
+               <h4>Sobre a Salesforce</h4>
+               <ul className={ classes.columnItemWrapper }>
+                  <li><a href="#">Nossa História</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Carreira</a></li>
+                  <li><a href="#">Salesforce.org</a></li>
+                  <li><a href="#">Dê nos o seu feedback</a></li>
+               </ul>
+            </div>
+         </div>   
       </footer>
-   );
-};
+   )
+}
 
-export default Footer;
+export default Footer
